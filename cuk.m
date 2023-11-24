ca1ce024b810c4306e415c34283ec45d7d482903
EdgarHdz-13 <85575704+EdgarHdz-13@users.noreply.github.com>
2023-11-24 03:34:25 -0600

    Fix but eq error

--- cuk.m
+++ cuk.m
@@ -6,7 +6,7 @@ opts.FreqUnits = 'Hz';
 s = tf('s');
 
 % Analisis Controlador%
-syms VL1 IL1 VC1 IC1 VL2 IL2 VC2 IC2 D RON R IO VO L1 L2 C1 C2
+syms VL1 IL1 VC1 IC1 VL2 IL2 VC2 IC2 D RON R L1 L2 C1 C2
 Dp = 1-D;
 IO = 3;
 VO = 5;
@@ -50,40 +50,51 @@ VC1 = double(VC1);
 eq16 = -IL2*D+IL1*Dp == 0;
 IL1 = double(solve(eq16,IL1));
 
-%% 
 
-DeIL = 0.30;
-eq5 = VL1 == vg-RL1*IL1-VC1-RC1*IC1-vd;
-eq6 = VL2 == -vd-RL2*IL2-VC2-RC2*IC2;
-eq7 = IC1 == IL1;
-eq8 = IC2 == IL2-VC2/R;
+eq1 = VL1 == vg-RL1*IL1-RON*(IL1-IC1);
+eq2 = VL2 == -RON*(IL1-IC1)+VC1+RC1*IC1-RL2*IL2-VC2-RC2*IC2;
+eq3 = IC1 == IL2;
+eq4 = IC2 == IL2-VC2/R;
 
-VL1 = solve(eq5,VL1);
-VL2 = solve(eq6,VL2);
-IC1 = solve(eq7,IC1);
-IC2 = solve(eq8,IC2);
+VL1 = double(solve(eq1,VL1));
+VL2 = double(solve(eq2,VL2));
+% IC1 == IL2;               % Se cumple
+% IC2 = solve(eq4,IC2);     % También se cumple
+%
+DeIL1 = 0.30;
+DeIL2 = 0.30;
+DeVC1 = 0.03;
+DeVC2 = 0.03;
 
-eq22 = (2*DeIL)/(D*T) == VL1/L1;
-%%
-eq6 = (2*0.30)/(0.302*(1/50000)) == (15-RL1*0.695-0.003*(0.695-3))/L1;
-L1 = solve(eq6,L1);
 
-eq7 = (2*0.30)/(0.302*(1/50000)) == (-0.003*(0.695-3)+15.460+1.5*-3-0.260*3-5)/L2;
-L2 = solve(eq7,L2);
+eq22 = (2*DeIL1)/(D*T) == VL1/L1;
+L1 = solve(eq22,L1);
 
-eq8 = 2*0.03 == (0.30*T)/(8*C1);
-C1 = solve(eq8,C1);
+eq23 = (2*DeIL2)/(D*T) == VL2/L2;
+L2 = solve(eq23,L2);
 
-eq9 = (2*0.03)/(D*T)==5/(R*C2);
-C2 = solve(eq9,C2);
+eq24 = 2*DeVC1 == (DeIL1*T)/(8*C1);
+C1 = solve(eq24,C1);
+
+eq25 = 2*DeVC2/(D*T) == VC2/(R*C2);
+C2 = solve(eq25,C2);
+
+% eq7 = (2*0.30)/(0.302*(1/50000)) == (-0.003*(0.695-3)+15.460+1.5*-3-0.260*3-5)/L2;
+% L2 = solve(eq7,L2);
+% 
+% eq8 = 2*0.03 == (0.30*T)/(8*C1);
+% C1 = solve(eq8,C1);
+% 
+% eq9 = (2*0.03)/(D*T)==5/(R*C2);
+% C2 = solve(eq9,C2);
 
 % Conversión de valores a double
 L1 = double(L1);
 L2 = double(L2);
 C1 = double(C1);
-C2 = -double(C2);
+C2 = double(C2);
 
-%% Matrices
+% Matrices
 Am = [  -((RL1+RON*D)/L1)   ,-(RON*D)/L1        ,(1-D)/L1   ,0;
         -(RON*D)/L2         ,(RL2+RON*D)/L2     ,D/L2       ,-1/L2;
         ((1-D)*T)/C1        ,-D/C1              ,0          ,0;
